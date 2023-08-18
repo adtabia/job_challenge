39fb22592717e7a8073fa4ebcaa8514057d305b4
Angel Daniel Tabia <angel.d.tabia@gmail.com>
2023-08-18 00:18:10 -0300

    - Fixed HiredEmployees datetime data type - Added a check for the table_name - Added GET Request for hires in 2021 divided by quarter - Added GET Request for departments with hires over the mean of 2021

--- app/models.py
+++ app/models.py
@@ -1,24 +1,24 @@
 from datetime import datetime
 from app import db
 
-class Department(db.Model):
+class Departments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     department = db.Column(db.String(50), nullable=False)
 
-class Job(db.Model):
+class Jobs(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     job = db.Column(db.String(50), nullable=False)
 
-class HiredEmployee(db.Model):    
+class HiredEmployees(db.Model):    
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
-    # datetime = db.Column(db.Datetime, default=datetime.utcnow)
-    datetime = db.Column(db.String(50))
-    department_id = db.Column(db.Integer, db.ForeignKey('department.id'))
-    job_id = db.Column(db.Integer, db.ForeignKey('job.id'))
+    datetime = db.Column(db.DateTime)
+    # datetime = db.Column(db.String(50))
+    department_id = db.Column(db.Integer, db.ForeignKey('departments.id'))
+    job_id = db.Column(db.Integer, db.ForeignKey('jobs.id'))
 
 model_cols = {
-    'department': ['id', 'department'],
-    'job': ['id', 'job'],
-    'hired_employee': ['id', 'name', 'datetime', 'department_id', 'job_id']
+    'departments': ['id', 'department'],
+    'jobs': ['id', 'job'],
+    'hired_employees': ['id', 'name', 'datetime', 'department_id', 'job_id']
 }
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,12 @@
-from flask import request
+from flask import request, jsonify
 from app import app, db
-from app.models import model_cols, Department, HiredEmployee, Job
+from app.models import model_cols, Departments, HiredEmployees, Jobs
 import csv
 import os
 import io
 import pandas as pd
 import dateutil
+from sqlalchemy import text
 
 @app.route('/batch-insert', methods=['POST'])
 def upload_and_batch_insert():
@@ -21,6 +22,8 @@ def upload_and_batch_insert():
     # Checking if the table name is correct
     if not table_name:
         return {"error": "Table name missing"}, 400
+    if not table_name in model_cols.keys():
+        return {"error": "Table name incorrect"}, 400
         
     rows_to_insert = []
     skipped_rows = 0
@@ -32,7 +35,7 @@ def upload_and_batch_insert():
     for index, row in df.iterrows():
     
         # Batch insert of departments data
-        if table_name == 'department':
+        if table_name == 'departments':
             # Data type validation
             try:
                 id_val = int(row['id'])
@@ -41,9 +44,9 @@ def upload_and_batch_insert():
                 invalid_rows += 1
                 continue
 
-            existing_ids = {row[0] for row in db.session.query(Department.id).all()}
+            existing_ids = {row[0] for row in db.session.query(Departments.id).all()}
             if id_val not in existing_ids:
-                new_entry = Department(id=id_val, department=department_val)
+                new_entry = Departments(id=id_val, department=department_val)
                 rows_to_insert.append(new_entry)
                 existing_ids.add(id_val)
             else:
@@ -55,7 +58,7 @@ def upload_and_batch_insert():
 
         
         # Batch insert of jobs data
-        if table_name == 'job':
+        elif table_name == 'jobs':
             # Data type validation
             try:
                 id_val = int(row['id'])
@@ -64,9 +67,9 @@ def upload_and_batch_insert():
                 invalid_rows += 1
                 continue
 
-            existing_ids = {row[0] for row in db.session.query(Job.id).all()}
+            existing_ids = {row[0] for row in db.session.query(Jobs.id).all()}
             if id_val not in existing_ids:
-                new_entry = Job(id=id_val, job=job_val)
+                new_entry = Jobs(id=id_val, job=job_val)
                 rows_to_insert.append(new_entry)
                 existing_ids.add(id_val)
             else:
@@ -78,22 +81,21 @@ def upload_and_batch_insert():
                 
 
         # Batch insert of hired employees data
-        if table_name == 'employees':
+        elif table_name == 'hired_employees':
             # Data type validation
             try:
                 id_val = int(row['id'])
                 name_val = str(row['name'])
-                # datetime_val = dateutil.parser.parse(row['datetime'])
-                datetime_val = str(row['datetime'])
+                datetime_val = dateutil.parser.parse(str(row['datetime']))
                 department_id_val = int(row['department_id'])
                 job_id_val = int(row['job_id'])
             except ValueError:
                 invalid_rows += 1
                 continue
             
-            existing_ids = {row[0] for row in db.session.query(HiredEmployee.id).all()}
+            existing_ids = {row[0] for row in db.session.query(HiredEmployees.id).all()}
             if id_val not in existing_ids:
-                new_entry = HiredEmployee(id=id_val, name=name_val, datetime=datetime_val, department_id=department_id_val, job_id=job_id_val)
+                new_entry = HiredEmployees(id=id_val, name=name_val, datetime=datetime_val, department_id=department_id_val, job_id=job_id_val)
                 rows_to_insert.append(new_entry)
                 existing_ids.add(id_val)
             else:
@@ -116,6 +118,89 @@ def upload_and_batch_insert():
         message += " {invalid_rows} rows were skipped due to invalid data types.".format(invalid_rows=invalid_rows)
 
     return {"message": message}, 200
+
+
+
+@app.route('/hires-quater-2021', methods=['GET'])
+def hires_by_quarter_2021_sql():
+    sql = text("""
+    SELECT 
+    d.department,
+    j.job,
+    CASE 
+        WHEN strftime('%m', e.datetime) BETWEEN '01' AND '03' THEN 'Q1'
+        WHEN strftime('%m', e.datetime) BETWEEN '04' AND '06' THEN 'Q2'
+        WHEN strftime('%m', e.datetime) BETWEEN '07' AND '09' THEN 'Q3'
+        WHEN strftime('%m', e.datetime) BETWEEN '10' AND '12' THEN 'Q4'
+    END AS quarter,
+    COUNT(e.id) AS num_hires
+FROM 
+    hired_employees e
+JOIN 
+    departments d ON e.department_id = d.id
+JOIN 
+    jobs j ON e.job_id = j.id
+WHERE 
+    strftime('%Y', e.datetime) = '2021'
+GROUP BY 
+    d.department, j.job, quarter
+ORDER BY 
+    d.department, j.job;
+    """)
+    # Get a connection and execute the SQL
+    with db.engine.connect() as connection:
+        result = connection.execute(sql)
+        rows = result.fetchall()
+
+    # Convert results to a table structure
+    table_header = ["Department", "Job", "Quarter", "Number of Hires"]
+    table_data = [list(row) for row in rows]
+
+    table = {
+        "header": table_header,
+        "data": table_data
+    }
+
+    return jsonify(table)
+
+
+
+@app.route('/hires-upper-avg', methods=['GET'])
+def hires_upper_average():
+    sql = text("""
+    WITH MeanHires AS (
+    SELECT AVG(hire_count) AS mean_hires
+    FROM (
+        SELECT department_id, COUNT(id) AS hire_count
+        FROM hired_employees
+        WHERE strftime('%Y', datetime) = '2021'
+        GROUP BY department_id
+    )
+)
+
+SELECT d.id, d.department AS name, COUNT(e.id) AS num_hires
+FROM departments d
+JOIN hired_employees e ON d.id = e.department_id
+WHERE strftime('%Y', e.datetime) = '2021'
+GROUP BY d.id, d.department
+HAVING COUNT(e.id) > (SELECT mean_hires FROM MeanHires)
+ORDER BY num_hires DESC;
+    """)
+    # Get a connection and execute the SQL
+    with db.engine.connect() as connection:
+        result = connection.execute(sql)
+        rows = result.fetchall()
+
+    # Convert results to a table structure
+    table_header = ["Id", "Department", "Number of Hires"]
+    table_data = [list(row) for row in rows]
+
+    table = {
+        "header": table_header,
+        "data": table_data
+    }
+
+    return jsonify(table)
 
 
 
